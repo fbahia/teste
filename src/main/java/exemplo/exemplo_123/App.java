a5a5005e746fdb50b6cd400c80d25d8f666ed580
Fabio Bahia <fbahia32@gmail.com>
2016-04-25 01:02:00 -0300

    commit inicial

---
+++ src/main/java/exemplo/exemplo_123/App.java
@@ -0,0 +1,14 @@
+package exemplo.exemplo_123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("teste");
+    }
+}
